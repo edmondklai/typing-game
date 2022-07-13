ac43cbdca1e906bf3a8a9932bf42e67ea395f4ec
Edmond <edmondklai@gmail.com>
2022-07-13 12:24:24 -0500

    feat(GameSurface): Display randomized game words

--- src/GameSurface.tsx
+++ src/GameSurface.tsx
@@ -1,10 +1,25 @@
+import Box from '@mui/material/Box';
+import { words } from './Data/words';
 import './GameSurface.css';
 
+
+
 function GameSurface() {
+  function shuffle(array: string[]): string[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   return (
     <div className="GameSurface">
-      GameSurface
+      <Box>
+        {shuffle(words).map((word: string) => {
+          return <span>{word} </span>
+        })}
+      </Box>
     </div>
   )
 }
